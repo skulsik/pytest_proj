fbf2e3806289a8bb2e180879592507c0b0aec042
skulsik <skulsik1@gmail.com>
2023-02-09 16:43:59 +0700

    С фикстурой

--- tests/test_dicts.py
+++ tests/test_dicts.py
@@ -1,21 +1,24 @@
 from utils import dicts
+import pytest
+
+@pytest.fixture
+def data():
+    return {'planet': 'mars', 'girl': 'nice', 'dog': 'bad'}
 
 
-def test_val_list_key():
-    data = {'planet': 'mars', 'girl': 'nice', 'dog': 'bad'}
+def test_val_list_key(data):
     assert dicts.val(data, 'girl') == 'nice'
 
 
-def test_val_list_key_default():
-    data = {'planet': 'mars', 'girl': 'nice', 'dog': 'bad'}
+def test_val_list_key_default(data):
     assert dicts.val(data, 'girl', 'dirty') == 'nice'
 
 
-def test_val_key_default():
+def test_val_key_default(data):
     data = {}
     assert dicts.val(data, 'dog', 'dirty') == 'dirty'
 
 
-def test_val_key():
+def test_val_key(data):
     data = {}
     assert dicts.val(data, 'planet') == 'ooops!'
